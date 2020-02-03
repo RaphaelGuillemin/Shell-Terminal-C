fdf92f50a181adf29047ba60aabc1d817b0f2d18
Raphael Guillemin <raphael.guillemin@umontreal.ca>
2020-02-03 12:55:43 -0500

    Initial files

---
+++ template.c
@@ -0,0 +1,18 @@
+#define true 1
+#define false 0
+#define bool int
+
+char* readLine() {}
+
+void shell() {
+
+}
+
+/*
+ * Dont change main!
+ *
+ * You may add unit tests here, but we will rip out main entirely when we grade your work.
+ */
+int main (void) {
+    shell();
+}
