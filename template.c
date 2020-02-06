780bae15712110184bb8aadb9e785e3b7660a7cc
Raphael Guillemin <raphael.guillemin@umontreal.ca>
2020-02-06 17:53:56 -0500

    Now reading input lines

--- template.c
+++ template.c
@@ -2,10 +2,48 @@
 #define false 0
 #define bool int
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 char* readLine() {}
 
 void shell() {
+    int lineSize = 20;
+    int character;
+    int pos = 0;
+    char* line = malloc(lineSize * sizeof(char));
+    if (line == NULL){
+        exit(-1);
+    }
+    do {
+        character = getchar();
 
+        //if line is longer than the lineSize
+        if (pos >= lineSize){
+            lineSize = lineSize * 2;
+            line = realloc(line,lineSize);
+        }
+
+        //adds characters to the line while it's not \n
+        if (character != 10 && character != EOF) {
+            line[pos] = character;
+            pos++;
+        } else {
+            line[pos] = '\0';
+            printf("Line : %s \n", line); //CHANGE THAT TO READLINE CALL
+            free(line);
+            line = malloc(lineSize * sizeof(char));
+            pos = 0;
+        }
+
+        // exit if user types exit
+        if (strcmp(line,"exit") == 0){
+            exit(1);
+        }
+
+    } while (1);
+    printf("Line : %s", line);
 }
 
 /*
